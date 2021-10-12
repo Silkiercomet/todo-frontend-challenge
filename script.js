c1a51eec40f9cd0e1cf1070cf024f718779ca64d
luis Colina <79709581+Silkiercomet@users.noreply.github.com>
2021-10-12 07:52:41 -0400

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,8 @@ background = document.querySelector("#black-background"),
 todoForm = document.querySelector("#todo-form"),
 todoInput = document.querySelector("#todo-content-input"),
 icon = document.querySelector("#icon-change"),
-checkmark = document.querySelectorAll(".checkmark")
+checkmark = document.querySelectorAll(".checkmark"),
+newArray = []
 
 
 const togglerMode = () => {
@@ -29,15 +30,23 @@ const togglerMode = () => {
     checkmark.forEach(a => a.classList.toggle("checkmark-dark"))
 }
 switcher.addEventListener("click", togglerMode)
+let checkedArray = [], unchekedArray = [];
+
+//add the li to an array
+
+const pushArray = (element, position) => {
+    newArray.push([element,position])
+}
 
 //dinamic add of li
-
+let counter = 0;
 const addLi = () => {
-    let counter = 0;
+    
     
     return function(x) {
         let newLi = document.createElement("li")
-        newLi.classList.add("todo__item")
+        newLi.classList.add(`todo__item`)
+        newLi.setAttribute("id",`item-num${counter}`)
         newLi.innerHTML = `
             <input type="checkbox" name="todo radio" id="checkbox-item-${counter}" class="radio radio2">
             <label for="checkbox-item-${counter}" class="con">
@@ -46,10 +55,25 @@ const addLi = () => {
             <span class="todo__text">${x}</span>
             <i class="cross"><img src="images/icon-cross.svg" alt=""></i>
         `;
+        pushArray(newLi,`item-num${counter}`)
         todoItemsColor.appendChild(newLi)
         counter += 1
         document.querySelector("#todo-content-input").value = ""
-        
+
+    }
+}
+
+const sortedArrays = () => {
+    checkedArray = [], unchekedArray = []
+    for(let i = 0; i < counter; i++){
+        if(newArray[i][0].querySelector(`#checkbox-item-${i}`).checked == true){
+            
+            console.log(newArray[i][0].querySelector(`#checkbox-item-${i}`).checked == true)
+            checkedArray.push(newArray[i])
+        }else if(newArray[i][0].querySelector(`#checkbox-item-${i}`).checked == false){
+            console.log(newArray[i][0].querySelector(`#checkbox-item-${i}`).checked == false)
+            unchekedArray.push(newArray[i])
+        }
     }
 }
 
@@ -65,77 +89,27 @@ const grabText = () => {
 
 const countTotal = () => document.querySelector(".todo__counterleft").textContent = `${document.querySelectorAll(".radio2").length} `
 
-const organizedList = (x) => {
-    let newArray = [], currentArrays = document.querySelectorAll(".radio2"),
-    activeElements = [],doneElements = []
 
-     currentArrays.forEach(a => {
-        if(a.checked !=  true){
-            
-            activeElements.push(a.id)
-        }
-    })
-
-      currentArrays.forEach(a => {
-        if( a.checked == true){
-            doneElements.push(a.id)
-        }
-    })
-    // todoItemsColor parent element of the items
-    const addActive = () => {
-        activeElements.forEach(x => {
-            todoItemsColor.appendChild(document.querySelector(`#${x}`))
-     
-         })
-    } 
-    const addCompleted = () => {
-        doneElements.forEach(x => {
-            todoItemsColor.appendChild(document.querySelector(`#${x}`))
-            x.checked = true
-         })
-    } 
-    const removeActive = () => {
-
-    } 
-    const removeCompleted = () => {
-
-    } 
-    switch (x) {
-        case 1:
-        //shows all the elements
-        addActive()
-        addCompleted()
-        break;
-
-        case 2:
-        //shows the active elements
-        addActive()
-        doneElements.forEach(x => {
-            document.querySelector(`#${x}`).parentElement.remove()
-         })
-        break;
-
-        case 3:
-        //shows the completed elements
-        addCompleted()
-        activeElements.forEach(x => {
-            document.querySelector(`#${x}`).parentElement.remove()
-            
-         })
-        break;
-    }
-
-    //1 shows all, 2 shows actives and 3 shows completed 
-    
-}
 
 const deleteSpecific = () => {
     let cross = document.querySelectorAll(".cross")
     cross.forEach(cross => {
 
         cross.addEventListener("click", function(){
+            newArray.forEach(a => {
+                if(a.indexOf(cross.parentElement.id) == 1){
+                    newArray.splice(newArray.indexOf(a),1)
+                    return a
+                }
+                if(a.indexOf(cross.parentElement.id) == 1){
+                    newArray.splice(newArray.indexOf(a),1)
+                    return a
+                }
+            })
+            
             cross.parentElement.remove()
             countTotal()
+            sortedArrays()
         })
         
     })
@@ -146,27 +120,76 @@ document.getElementById("first_radio").addEventListener("click", function(){
     compose(grabText,temp)()
     deleteSpecific()
     countTotal()
-    
+    sortedArrays()
+    document.querySelectorAll(".radio2").forEach(e => {
+        e.addEventListener("click", sortedArrays)
+    })
 })
 todoInput.addEventListener("keyup", e => {
-    if (e.keyCode == 66|| e.keyCode == 13) {
-        e.preventDefault();
+    e.preventDefault();
+    if (e.keyCode == 13) {
+        
         compose(grabText,temp)()
+        deleteSpecific()
+        countTotal()
+        sortedArrays()
+        document.querySelectorAll(".radio2").forEach(e => {
+            e.addEventListener("click", sortedArrays)
+        })
        }
-       console.log("worked")
+      console.log(e)
 })
 
+const footerState = () => {
+    for (let index = 0; index < 3; index++) {
+        document.querySelector("#footer__state").children[index].classList.remove('active')
+        
+    }
+    
+}
+
 document.querySelector("#show__active").addEventListener("click", function(){
-    console.log("listen")
-    organizedList(2)
+    //unchekedArray todo__items
+    todoItemsColor.innerHTML = ""
+    for (let i = 0; i <= unchekedArray.length-1; i++) {
+        todoItemsColor.appendChild(unchekedArray[i][0])
+    }
+    footerState()
+    document.querySelector("#footer__state").children[1].classList.toggle("active")
 })
+
+
 document.querySelector("#show__completed").addEventListener("click", function(){
-    console.log("listen")
-    organizedList(3)
+    todoItemsColor.innerHTML = ""
+    for (let i = 0; i <= checkedArray.length-1; i++) {
+        todoItemsColor.appendChild(checkedArray[i][0])
+    }
+    footerState()
+    document.querySelector("#footer__state").children[2].classList.toggle("active")
 })
+
+
 document.querySelector("#all").addEventListener("click", function(){
-    console.log("listen")
-    organizedList(1)
+    todoItemsColor.innerHTML = ""
+    for (let i = 0; i <= newArray.length-1; i++) {
+        todoItemsColor.appendChild(newArray[i][0])
+    }
+    footerState()
+    document.querySelector("#footer__state").children[0].classList.toggle("active")
+})
+
+
+btn.addEventListener("click", function(){
+    todoItemsColor.innerHTML = ""
+    checkedArray = []
+    newArray = [...unchekedArray]
+    for (let i = 0; i <= newArray.length-1; i++) {
+        todoItemsColor.appendChild(newArray[i][0])
+    }
+    countTotal()
+    
 })
 const compose = (f, g) => (data) => g(f(data));
 const temp = addLi()
+
+
